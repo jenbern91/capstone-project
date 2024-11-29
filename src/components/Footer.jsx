c0a88a6a8b4f2aa4dbf58350c8fa03a5b3839d8a
jenbern91 <jenbern91@gmail.com>
2024-11-29 11:15:47 +0800

    second commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+   <>
+   <nav className='site-map'>
+   <h4>Sitemap</h4>
+
+
+   </nav>
+   <ul>
+      <li><a>Home</a></li>
+      <li><a>Menu</a></li>
+      <li><a>Reservations</a></li>
+      <li><a>Order Online</a></li>
+      <li><a>Login</a></li>
+    </ul>
+
+
+    <div className='contact-us'>
+    <h4>Contact Us</h4>
+    <p> 123 Fake Ave, Chicago, IL 60602</p>
+    <p> +1 (012) 345-6789</p>
+    <p> info@littlelemon.com</p>
+    </div>
+
+    <div className='connect-with-us'>
+      
+
+    </div>
+   </>
+  )
+}
+
+export default Footer
---
+++ src/components/Nav.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav>
+    <img src='logo.svg'></img>
+    <ul>
+      <li><a>Home</a></li>
+      <li><a>Menu</a></li>
+      <li><a>Reservations</a></li>
+      <li><a>Order Online</a></li>
+      <li><a>Login</a></li>
+    </ul>
+    </nav>
+  )
+}
+
+export default Nav
