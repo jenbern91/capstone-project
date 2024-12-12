ee5f6ee190447078ac412185e16bdd7b3d276ef8
Jennifer <jenbern91@gmail.com>
2024-12-12 09:58:25 +0800

    dec 12

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,12 +6,12 @@ const Hero = () => {
     <div className="hero">
       <div className="box-1">
             <h1>Little Lemon</h1>
-            <h3 className='location'>Chicago</h3>
+                <h3 className='location'>Chicago</h3>
             <p className='description'>We are a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
             <button className='reserve-button'>Reserve a Table</button>
 
       </div>
-      <div className="box-2"> 
+      <div className="box-2">    
            <img className='showcase-image' src={showcaseImage}/>
       </div>
       
