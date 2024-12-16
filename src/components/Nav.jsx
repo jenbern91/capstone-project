d8dcf9f52110cfe8bf6aefab4237b4e327601f2a
jenbern91 <jenbern91@gmail.com>
2024-12-16 22:12:30 +0800

    dec16

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,33 +1,49 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../assets/layouts/Nav.css';
 
 const Nav = () => {
-  return (
-    <nav className='navbar' >
-        <div className='nav-container'>
-          <img src='Logo.svg' />
-      <ul>
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='#'>Menu</Link>
-        </li>
-        <li>
-          <Link to='/booking'>Reservations</Link>
-        </li>
-        <li>
-          <Link to='#'>Order Online</Link>
-        </li>
-        <li>
-          <Link to='#'>Login</Link>
-        </li>
-       
-    
-      </ul>
-      </div>
-      </nav>
-      )
-}
+  const [isOpen, setIsOpen] = useState(false);
 
-export default Nav
+  const toggleMenu = () => {
+    console.log('toggleMenu called');
+    setIsOpen(!isOpen);
+    console.log('isOpen:', !isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <nav className='navbar'>
+      <div className="hamburger" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+      <div className='nav-container'>
+        <img src='Logo.svg' alt='Logo' className='logo'/>
+        <ul className={`nav-links ${isOpen ? 'active' : ''}`}>
+          <li>
+            <Link to='/' onClick={closeMenu}>Home</Link>
+          </li>
+          <li>
+            <Link to='#' onClick={closeMenu}>Menu</Link>
+          </li>
+          <li>
+            <Link to='/booking' onClick={closeMenu}>Reservations</Link>
+          </li>
+          <li>
+            <Link to='#' onClick={closeMenu}>Order Online</Link>
+          </li>
+          <li>
+            <Link to='#' onClick={closeMenu}>Login</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
